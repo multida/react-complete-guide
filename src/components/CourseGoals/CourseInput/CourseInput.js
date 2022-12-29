68cfff0c6e5f03aa3b1cf4e3b8206fe1d7b6a8c9
김다예 <wow_da65@naver.com>
2022-12-29 23:06:51 +0900

    💄 styled-component 기본

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,39 @@
 import React, { useState } from "react";
-
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+    &:focus {
+      outline: none;
+      background: #fad0ec;
+      border-color: #8b005d;
+    }
+  }
+
+  &.inValid label {
+    color: red;
+  }
+
+  &.inValid input {
+    background-color: salmon;
+    border-color: red;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -27,10 +59,11 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? "inValid" : ""}`}>
+      <FormControl className={!isValid && "inValid"}>
+        {/* className={!isValid && "inValid"} */}
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
